d7cbb3839abe92d537d0d1b98baca5050334a821
Elvis Knelson <elfishpro@hotmail.com>
2020-11-04 17:07:10 -0600

    add product_oder model

---
+++ db/migrate/20201104230615_create_product_orders.rb
@@ -0,0 +1,11 @@
+class CreateProductOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :product_orders do |t|
+      t.integer :product_id
+      t.integer :order_id
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
