06646f44943a95f146154246d283022f5154a49e
Elvis Knelson <elfishpro@hotmail.com>
2020-11-04 16:32:27 -0600

    add categoryid to product table

---
+++ db/migrate/20201104223028_add_categoryid_to_product.rb
@@ -0,0 +1,5 @@
+class AddCategoryidToProduct < ActiveRecord::Migration[6.0]
+  def change
+    add_column :products, :category_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_221440) do
+ActiveRecord::Schema.define(version: 2020_11_04_223028) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_221440) do
     t.string "name"
     t.integer "price"
     t.integer "quantity"
+    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
