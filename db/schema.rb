1609a58f848168d2453c0f444f68bd5be10761ef
Elvis Knelson <elfishpro@hotmail.com>
2020-11-04 21:13:21 -0600

    add description column to product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_232047) do
+ActiveRecord::Schema.define(version: 2020_11_05_031248) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_232047) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+    t.string "description"
   end
 
   create_table "provinces", force: :cascade do |t|
