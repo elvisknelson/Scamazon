a836cfd7dc99fd55ee89352edebc88a649d65b2e
Elvis Knelson <elfishpro@hotmail.com>
2020-11-06 15:31:20 -0600

    update views

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -5,5 +5,6 @@ class ProductController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,9 @@
 class SearchController < ApplicationController
   def index
+    @search = params["search"]
+    if @search.present?
+      @name = @search["name"]
+      @products = Product.where("name LIKE ?", "%#{@name}%")
+    end
   end
 end
