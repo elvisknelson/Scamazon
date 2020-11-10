2c7a0356cefd39564762ce26eda3603423d4a48e
Elvis Knelson <elfishpro@hotmail.com>
2020-11-10 15:05:09 -0600

    move session variable to application controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,4 @@
 class ProductController < ApplicationController
-  before_action :initialize_session
-  before_action :load_cart
 
   def index
     @categories = Category.where.not(name: 'All')
@@ -9,29 +7,5 @@ class ProductController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-  end
-
-  def add_to_cart
-    id = params[:id].to_i
-
-    session[:cart] << id unless session[:cart].include?(id)
-    redirect_to root_path
-  end
-
-  def remove_from_cart
-    id = params[:id].to_i
-
-    session[:cart].delete(id)
-    redirect_to root_path
-  end
-
-  private
-
-  def load_cart
-    @cart = Product.find(session[:cart])
-  end
-
-  def initialize_session
-    session[:cart] ||= []
   end
 end
