7d48f02b16f1e023c22429dd958c1ac5215a140d
Elvis Knelson <elfishpro@hotmail.com>
2020-11-10 14:48:04 -0600

    update views

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,7 +1,10 @@
 class CategoryController < ApplicationController
   def index
+    @categories = Category.all()
   end
 
   def show
+    @category = Category.where(id: params[:id]).first
+    @products = Product.where(category_id: params[:id])
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,10 @@
 class SearchController < ApplicationController
   def index
-    @search = params["search"]
-    if @search.present?
-      @name = @search["name"]
-      @products = Product.where("name LIKE ?", "%#{@name}%")
+    if (Category.find(params["category"]).name == 'All')
+      @products = Product.where("name LIKE ?", "%#{params[:search_term]}%")
+    else
+      @products = Product.where("name LIKE ? AND category_id LIKE ?", "%#{params[:search_term]}%", "%#{params['category']}%")
     end
+
   end
 end
