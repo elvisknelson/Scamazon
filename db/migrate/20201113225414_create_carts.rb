bf3304c47c955a042e3ffa843848f2cbe832c992
Elvis Knelson <elfishpro@hotmail.com>
2020-11-13 16:54:22 -0600

    add cart model

---
+++ db/migrate/20201113225414_create_carts.rb
@@ -0,0 +1,11 @@
+class CreateCarts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :carts do |t|
+      t.integer :product_id
+      t.integer :quantity
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
