d74494fb1d37280258c53e7400a709c956724a37
Elvis Knelson <elfishpro@hotmail.com>
2020-11-17 22:14:23 -0600

    add sort options to nav

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -5,6 +5,20 @@ class ProductController < ApplicationController
     @products = Product.all()
   end
 
+  def sale
+    @categories = Category.where.not(name: 'All')
+    @products = Product.where(on_sale: true)
+  end
+
+  def rating
+    @categories = Category.where.not(name: 'All')
+    @products = Product.where("rating >= #{params[:rating]}")
+  end
+
+  def new
+    @products = Product.where("created_at > ?", 3.days.ago)
+  end
+
   def show
     @product = Product.find(params[:id])
   end
