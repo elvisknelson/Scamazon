090e4f8635c7d00f1ea6d3e1ea337dde0a678a73
Elvis Knelson <elfishpro@hotmail.com>
2020-11-17 23:10:23 -0600

    add hst column to province table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_001651) do
+ActiveRecord::Schema.define(version: 2020_11_18_050957) do
 
   create_table "about_us", force: :cascade do |t|
     t.string "header"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_001651) do
     t.decimal "gst"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "hst"
   end
 
   create_table "users", force: :cascade do |t|
