0188980250523a93e8a41773f4d5546992988984
Elvis Knelson <elfishpro@hotmail.com>
2020-11-18 14:00:13 -0600

    fix user account bugs

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -20,9 +20,13 @@ class AccountController < ApplicationController
   def sign_up
     if params[:user_name]
       if (params[:password_1] == params[:password_2])
-        user = User.create(username: params[:user_name], password: params[:password_1])
-        session[:user] = user.id
-        redirect_to root_path
+        unless User.exists?(username: params[:user_name])
+          user = User.create(username: params[:user_name], province_id: 7, street_address: params[:address], password: params[:password_1])
+          session[:user] = user.id
+          redirect_to root_path
+        else
+          redirect_to account_sign_in_path
+        end
       else
         redirect_to account_index_path
       end
