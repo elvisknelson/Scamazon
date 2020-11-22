693e85b968088c617cf933d3d8ebb0bb737f6bcc
Elvis Knelson <elfishpro@hotmail.com>
2020-11-22 17:03:29 -0600

    add total price and tax to cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,11 @@
 class CartController < ApplicationController
   def index
+    if session[:user]
+      @user = User.find(session[:user])
+    else
+      @user = User.find(23) #TODO FIX THIS
+    end
+
+    @total = 0.00
   end
 end
